c2a415ff7af3b56f09611a090b7fe327fe4d9764
Qvisty <jgq@live.dk>
2022-08-31 16:59:22 +0200

    Update MainWindow.xaml.cs
    mit bud nr 1

--- Kortspil/MainWindow.xaml.cs
+++ Kortspil/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Security.Policy;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -21,20 +23,96 @@ namespace Kortspil
             int kortnummer = Convert.ToInt32(Kort.Text);
             string filnavn = FindBillede(kortnummer);
             string url = $"/Billeder/{filnavn}";
-            Uri uri = new (url, UriKind.Relative);
+            Uri uri = new(url, UriKind.Relative);
             BitmapImage image = new(uri);
-           
+
             Billede.Source = image;
-            
+
         }
 
         private string FindBillede(int kortnummer)
         {
-            string resultat = "2-Spar.jpg";
 
-            // Skriv din løsning her...
+            string suit = "";
+            string kort = "";
+            string kortnavn = "";
+            bool fejl = false;
 
-            return resultat;
+            // kort pr.kulør
+            int sæt = 13;
+
+            // Finder kulør
+            if (1 <= kortnummer && kortnummer <= sæt)
+            {
+                suit = "Spar";
+            }
+            else if (sæt + 1 <= kortnummer && kortnummer <= 2 * sæt)
+            {
+                suit = "Ruder";
+                kortnummer -= sæt;
+            }
+            else if (2 * sæt + 1 <= kortnummer && kortnummer <= 3 * sæt)
+            {
+                suit = "Klør";
+                kortnummer -= 2 * sæt;
+            }
+            else if (3 * sæt + 1 <= kortnummer && kortnummer <= 4 * sæt)
+            {
+                suit = "Hjerter";
+                kortnummer -= 3 * sæt;
+            }
+            else
+            {
+                fejl = true;
+
+            }
+
+            // Finder kort
+            if (kortnummer == 1)
+            {
+                kort = "Es";
+            }
+            else if (kortnummer == 11)
+            {
+                kort = "Knægt";
+            }
+            else if (kortnummer == 12)
+            {
+                kort = "Dame";
+            }
+            else if (kortnummer == 13)
+            {
+                kort = "Konge";
+            }
+            else if (kortnummer >= 2 && kortnummer <= 9)
+            {
+                kort = Convert.ToString(kortnummer);
+            }
+            else
+            {
+                fejl = true;
+            }
+
+            // Tjekker for fejl
+            if (fejl == true)
+            {
+                // jeg ved fra google, at man ikke logger sådan i VS, men jeg prøvede at tænke det ind...
+                Console.WriteLine("Der skete en fejl. Husk at indtaste et helt tal i intervallet 1 - 52");
+            }
+            else
+            {
+                // Sammensætter filnavnet hvis der ikke er fejl
+                kortnavn += kort;
+                kortnavn += "-";
+                kortnavn += suit;
+                kortnavn += ".jpg";
+                Console.Write("Filnavn:\t");
+
+                return kortnavn;
+                
+            }
+
+
         }
     }
 }
